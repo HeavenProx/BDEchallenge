b58ed8b65c4cdf78570c483178027f25845f4505
Morgan Kpassi <>
2024-02-19 16:39:24 +0100

    add folder database

--- public/index.php
+++ public/index.php
@@ -7,30 +7,17 @@ use App\Controller\ProductController;
 use App\Routing\Exception\RouteNotFoundException;
 use App\Routing\Route;
 use App\Routing\Router;
-
-$dbConfig = parse_ini_file(__DIR__ . '/../config/db.ini');
-
-if ($dbConfig === false) {
-    echo "Fichier de configuration de la base de données introuvable, créez un fichier db.ini dans config/ (voir README)";
-    exit;
-}
-
-[
-    'DB_HOST'     => $host,
-    'DB_PORT'     => $port,
-    'DB_NAME'     => $dbName,
-    'DB_CHARSET'  => $charset,
-    'DB_USER'     => $user,
-    'DB_PASSWORD' => $password
-] = $dbConfig;
+use App\Database\DBConnector; // Importez la classe DBConnector
 
 try {
-    $dsn = "mysql:host=$host;port=$port;dbname=$dbName;charset=$charset";
-    $pdo = new PDO($dsn, $user, $password);
+    // Utilisez la classe DBConnector pour établir la connexion à la base de données
+    $dbConnector = new DBConnector();
+    $pdo = $dbConnector->getPDO();
 } catch (PDOException) {
     echo "Erreur lors de la connexion à la base de données";
     exit;
 }
+
 
 $router = new Router();
 
--- src/Database/DBConnector.php
+++ src/Database/DBConnector.php
@@ -11,8 +11,11 @@ class DBConnector
     public function __construct()
     {
         $config = parse_ini_file(__DIR__ . '/../../config/db.ini');
-        $dsn = "mysql:host={$config['host']};dbname={$config['dbname']}";
-        $this->pdo = new PDO($dsn, $config['username'], $config['password']);
+        
+        // Utilisez les clés correctes du fichier db.ini
+        $dsn = "mysql:host={$config['DB_HOST']};dbname={$config['DB_NAME']};charset={$config['DB_CHARSET']}";
+        
+        $this->pdo = new PDO($dsn, $config['DB_USER'], $config['DB_PASSWORD']);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
