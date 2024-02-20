78afa80f0f68afcff752061f1786ead1a9ca7014
Enricima <mathis.enrici@gmail.com>
2024-02-20 09:46:57 +0100

    Correction DBConnector

--- src/Database/DBConnector.php
+++ src/Database/DBConnector.php
@@ -13,7 +13,7 @@ class DBConnector
         $config = parse_ini_file(__DIR__ . '/../../config/db.ini');
         
         // Utilisez les clés correctes du fichier db.ini
-        $dsn = "mysql:host={$config['DB_HOST']};dbname={$config['DB_NAME']};charset={$config['DB_CHARSET']}";
+        $dsn = "mysql:host={$config['DB_HOST']};port={$config['DB_PORT']};dbname={$config['DB_NAME']};charset={$config['DB_CHARSET']}";
         
         $this->pdo = new PDO($dsn, $config['DB_USER'], $config['DB_PASSWORD']);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
