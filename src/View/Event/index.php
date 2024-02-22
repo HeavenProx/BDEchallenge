dd25368b5e91521db024b80076a3b0b4a67486f7
Hugo <hugo.duperthuy@outlook.fr>
2024-02-22 18:05:49 +0100

    ajout page detail pour events

--- src/View/Event/index.php
+++ src/View/Event/index.php
@@ -80,48 +80,46 @@
 </div>
 
 
-    <!-- Affichage des utilisateurs -->
-    <ul id="events-list" class="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <?php foreach ($events as $event): ?>
-            <li class="bg-white p-4 rounded shadow">
-                <p class="text-xl font-bold mb-2 text-gray-800"><?php echo $event['name'] . ' - ' . $event['eventDate']; ?></p>
-                <p class="text-lg text-gray-500 mb-2"><?php echo $event['category'] . ' à ' . $event['location']; ?></p>
-                <p class="text-gray-700"><?php echo $event['description']; ?></p>
+<!-- Affichage des utilisateurs -->
+<ul id="events-list" class="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <?php foreach ($events as $event): ?>
+        <li class="bg-white p-4 rounded shadow">
+            <p class="text-xl font-bold mb-2 text-gray-800"><?php echo $event['name'] . ' - ' . $event['eventDate']; ?></p>
+            <p class="text-lg text-gray-500 mb-2"><?php echo $event['category'] . ' à ' . $event['location']; ?></p>
+            <p class="text-gray-700"><?php echo $event['description']; ?></p>
 
-                <?php if(isset($_SESSION['logged']) && $_SESSION['logged'] == true): ?>
-                    <?php if ($_SESSION['user']['role'] == 'Admin' || $_SESSION['user']['role'] == 'BDE'): ?>
-                        <a href="/event/edit/<?php echo $event['eventNumber']; ?>" class="bg-blue-900 text-white hover:bg-yellow-500 hover:text-blue-900 transition px-8 py-2 rounded-md cursor-pointer inline-block mt-4">Modifier</a>
-                        <a href="/event/delete/<?php echo $event['eventNumber']; ?>" onclick="return confirm('Are you sure?')" class="bg-red-900 text-white hover:bg-blue-500 hover:text-white transition px-8 py-2 rounded-md cursor-pointer inline-block mt-4">Supprimer</a>
-                    <?php endif; ?>
+            <?php if(isset($_SESSION['logged']) && $_SESSION['logged'] == true): ?>
+                <?php if ($_SESSION['user']['role'] == 'Admin' || $_SESSION['user']['role'] == 'BDE'): ?>
+                    <a href="/event/edit/<?php echo $event['eventNumber']; ?>" class="bg-blue-900 text-white hover:bg-yellow-500 hover:text-blue-900 transition px-8 py-2 rounded-md cursor-pointer inline-block mt-4">Modifier</a>
+                    <a href="/event/delete/<?php echo $event['eventNumber']; ?>" onclick="return confirm('Are you sure?')" class="bg-red-900 text-white hover:bg-blue-500 hover:text-white transition px-8 py-2 rounded-md cursor-pointer inline-block mt-4">Supprimer</a>
                 <?php endif; ?>
-                <?php if(isset($_SESSION['logged']) && $_SESSION['logged'] == true): ?>
-                    <?php if (!$wishlistButtons[$event['eventNumber']]): ?>
-                        <!-- Affiche le bouton "Ajouter aux Favoris" si l'événement n'est pas dans la wishlist -->
-                        <a href="/wishlist/add/<?php echo $event['eventNumber']; ?>" class="bg-blue-900 text-white hover:bg-yellow-500 hover:text-blue-900 transition px-5 py-2 rounded-md cursor-pointer inline-block mt-4">Ajouter aux Favoris</a>
-                    <?php else: ?>
-                        <!-- Affiche le bouton "Enlever des Favoris" si l'événement est dans la wishlist -->
-                        <a href="/wishlist/delete/<?php echo $event['eventNumber']; ?>" class="bg-red-900 text-white hover:bg-blue-500 hover:text-white transition px-5 py-2 rounded-md cursor-pointer inline-block mt-4">Enlever des Favoris</a>
-                    <?php endif; ?>
-                    Boutons pour ajouter ou supprimer des participants
-                    <?php if (!$eventParticipants[$event['eventNumber']]): ?>
-                        <!-- Affiche le bouton "Ajouter participant" si l'utilisateur n'est pas dans la liste des participants -->
-                        <a href="/event/add-participant/<?php echo $event['eventNumber']; ?>" class="bg-green-500 text-white hover:bg-green-700 px-5 py-2 rounded-md cursor-pointer inline-block mt-4">Participer</a>
-                    <?php else: ?>
-                        <!-- Affiche le bouton "Retirer participant" si l'utilisateur est dans la liste des participants -->
-                        <a href="/event/remove-participant/<?php echo $event['eventNumber']; ?>" class="bg-red-500 text-white hover:bg-red-700 px-5 py-2 rounded-md cursor-pointer inline-block mt-4">Ne plus participer</a>
-                    <?php endif; ?>
+            <?php endif; ?>
+            <?php if(isset($_SESSION['logged']) && $_SESSION['logged'] == true): ?>
+                <?php if (!$wishlistButtons[$event['eventNumber']]): ?>
+                    <!-- Affiche le bouton "Ajouter aux Favoris" si l'événement n'est pas dans la wishlist -->
+                    <a href="/wishlist/add/<?php echo $event['eventNumber']; ?>" class="bg-blue-900 text-white hover:bg-yellow-500 hover:text-blue-900 transition px-5 py-2 rounded-md cursor-pointer inline-block mt-4">Ajouter aux Favoris</a>
                 <?php else: ?>
-                    <a href="/login"><button class="bg-blue-900 text-white hover:bg-yellow-500 hover:text-blue-900 transition px-8 py-2 rounded-md cursor-pointer inline-block mt-4">Se connecter pour participer</button></a>
+                    <!-- Affiche le bouton "Enlever des Favoris" si l'événement est dans la wishlist -->
+                    <a href="/wishlist/delete/<?php echo $event['eventNumber']; ?>" class="bg-red-900 text-white hover:bg-blue-500 hover:text-white transition px-5 py-2 rounded-md cursor-pointer inline-block mt-4">Enlever des Favoris</a>
                 <?php endif; ?>
-            </li>
-        <?php endforeach; ?>
-    </ul>
+                Boutons pour ajouter ou supprimer des participants
+                <?php if (!$eventParticipants[$event['eventNumber']]): ?>
+                    <!-- Affiche le bouton "Ajouter participant" si l'utilisateur n'est pas dans la liste des participants -->
+                    <a href="/event/add-participant/<?php echo $event['eventNumber']; ?>" class="bg-green-500 text-white hover:bg-green-700 px-5 py-2 rounded-md cursor-pointer inline-block mt-4">Participer</a>
+                <?php else: ?>
+                    <!-- Affiche le bouton "Retirer participant" si l'utilisateur est dans la liste des participants -->
+                    <a href="/event/remove-participant/<?php echo $event['eventNumber']; ?>" class="bg-red-500 text-white hover:bg-red-700 px-5 py-2 rounded-md cursor-pointer inline-block mt-4">Ne plus participer</a>
+                <?php endif; ?>
+            <?php else: ?>
+                <a href="/login"><button class="bg-blue-900 text-white hover:bg-yellow-500 hover:text-blue-900 transition px-8 py-2 rounded-md cursor-pointer inline-block mt-4">Se connecter pour participer</button></a>
+            <?php endif; ?>
+        </li>
+    <?php endforeach; ?>
+</ul>
 <div class="container mx-auto">
     <a href="/events/prevp" class="bg-blue-900 text-white hover:bg-yellow-500 hover:text-blue-900 transition px-8 py-2 rounded-md cursor-pointer inline-block mt-4">Page précédente</a>
     <a href="/events/nextp" class="bg-blue-900 text-white hover:bg-yellow-500 hover:text-blue-900 transition px-8 py-2 rounded-md cursor-pointer inline-block mt-4">Page suivante</a>
 </div>
-</div>
-
 
 </body>
 </html>
