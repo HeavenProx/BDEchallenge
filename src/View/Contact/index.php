a2f5ab3ad2d7d07a0a0219755ee12e85a78a852e
Morgan Kpassi <>
2024-02-22 22:24:07 +0100

    fix

--- src/View/Contact/index.php
+++ src/View/Contact/index.php
@@ -62,5 +62,8 @@
         <button type="submit" class="bg-yellow-500 text-blue-900 px-6 py-3 rounded-md cursor-pointer hover:bg-yellow-400">Envoyer</button>
 
     </form>
+    <div class="py-20 w-full">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2784.1726218771773!2d4.863066377206497!3d45.747685171079986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4ea7410c5175b%3A0xa0741b8940b6aded!2sCampus%20Sciences-U%20Lyon!5e0!3m2!1sfr!2sfr!4v1708636488258!5m2!1sfr!2sfr" width="600" height="300" style="border:0; width:100%" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
 </body>
 </html>
